c813fdfce8d32418133266eba1cce57d646694c1
laura.szekeres <szekereslaura98@gmail.com>
2025-06-03 18:57:00 +0200

    Initial commit
    Created project structure with Maven build. Added README and LICENSE files. Added basic dictionary.txt with 500 words.

---
+++ README.md
@@ -0,0 +1,12 @@
+# Wordle CLI
+
+This is a command line version of the original Wordle game.
+
+## Overview
+*On progress*
+
+## Requirements
+
+- Java 21+ 
+- Maven 3.8+
+- Developed and tested on Ubuntu 22.04.5 LTS
---
+++ src/main/java/com/example/wordle/WordleCliApplication.java
@@ -0,0 +1,13 @@
+package com.example.wordle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordleCliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordleCliApplication.class, args);
+	}
+
+}
